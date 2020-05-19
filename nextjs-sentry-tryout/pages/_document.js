5be2befc887754c1da23c3468af0b789bab793df
louiscklaw <louiscklaw@gmail.com>
2020-05-19 23:01:36 +0800

    adding sentry tracking example,.

---
+++ nextjs-sentry-tryout/pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script src="https://browser.sentry-cdn.com/5.15.5/bundle.min.js" integrity="sha384-wF7Jc4ZlWVxe/L8Ji3hOIBeTgo/HwFuaeEfjGmS3EXAG7Y+7Kjjr91gJpJtr+PAT" crossOrigin="anonymous"></script>
+          <script src="init_sentry.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+
+          <button onClick="(e) => {test(e)}" >
+          ttt
+          </button>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ nextjs-sentry-tryout/public/init_sentry.js
@@ -0,0 +1,3 @@
+Sentry.init({ dsn: 'https://1c68a3de3a56488ab1f534222afb8ae5@o321364.ingest.sentry.io/5246508' });
+
+notExist();
