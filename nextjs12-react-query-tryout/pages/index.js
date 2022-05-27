852cab0a03eda8ce8baf0eb5a4cb5f92574f14e6
Louis Law <louiscklaw@users.noreply.github.com>
2022-05-27 19:42:14 +0800

    update nextjs12-helloworld,

--- nextjs12-react-query-tryout/pages/index.js
+++ nextjs12-react-query-tryout/pages/index.js
@@ -1,15 +1,10 @@
-import Head from "next/head";
-import axios from "axios";
-import {
-  useQuery,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
+import Head from 'next/head'
+import axios from 'axios'
+import { useQuery, useQueryClient, QueryClient, QueryClientProvider } from 'react-query'
 
-import TestComponent from "../components/TestComponent";
+import TestComponent from '../components/TestComponent'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export default function Home() {
   return (
@@ -101,8 +96,8 @@ export default function Home() {
             border-radius: 5px;
             padding: 0.75rem;
             font-size: 1.1rem;
-            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+              Bitstream Vera Sans Mono, Courier New, monospace;
           }
 
           .grid {
@@ -162,9 +157,8 @@ export default function Home() {
           body {
             padding: 0;
             margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-              sans-serif;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+              Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
           }
 
           * {
@@ -173,5 +167,5 @@ export default function Home() {
         `}</style>
       </div>
     </QueryClientProvider>
-  );
+  )
 }
