b4713685b96db02ae7c666cbbd7c93a1f339ea73
Louis Law <louiscklaw@users.noreply.github.com>
2022-06-07 16:39:23 +0800

    update nextjs-portfolio-tryout,

--- nextjs-portfolio-tryout/cypress/e2e/helloworld/localhost.cy.js
+++ nextjs-portfolio-tryout/cypress/e2e/helloworld/localhost.cy.js
@@ -1,8 +1,27 @@
-describe('viewports', () => {
-  it('localhost self test!', () => {
-    cy.viewport('ipad-2')
-    cy.visit('http://localhost:3000')
-    cy.wait(500)
-    cy.compareSnapshot('localhost_self_test', 0.0)
+describe(`test viewports`, () => {
+  let preset_list = [
+    'ipad-2',
+    'ipad-mini',
+    'iphone-8',
+    'iphone-x',
+    'iphone-se2',
+    'macbook-13',
+    'macbook-15',
+    'macbook-16',
+    'samsung-note9',
+    'samsung-s10',
+  ]
+
+  let address_list = [`http://localhost:3000`, `http://localhost:3000/work/project1`]
+
+  it(`landing page self test!`, () => {
+    preset_list.map(preset => {
+      address_list.map(address => {
+        cy.viewport(preset)
+        cy.visit(address)
+        cy.wait(500)
+        cy.compareSnapshot(`${preset}_localhost_self_test_${address.replace(/S/, '_')}`, 0.0)
+      })
+    })
   })
 })
