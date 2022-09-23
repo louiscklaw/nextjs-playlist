ea89b6c23bfbe176889c9576f683846113957d66
louiscklaw <louiscklaw@users.noreply.github.com>
2022-09-23 19:48:24 +0800

    update nextjs-portfolio-tryout,

--- nextjs-portfolio-tryout/pages/test_page/project1/index.jsx
+++ nextjs-portfolio-tryout/pages/test_page/project1/index.jsx
@@ -1,11 +1,33 @@
-import { Typography } from '@mui/material';
+import { Typography, Grid, Box } from '@mui/material';
 import Link from 'next/link';
+import NameCard from '../NameCard';
+import { use100vh } from 'react-div-100vh';
 
 export default function Project1() {
+  let height_100vh = use100vh();
+
   return (
     <>
-      <Typography>Project1</Typography>
-      <Link href="/test_page/page1">back</Link>
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        style={{ width: '100vw', height: height_100vh }}
+      >
+        <Grid item xs={3} container direction="column">
+          <Box>
+            <NameCard />
+          </Box>
+          <Link href="/test_page/page1">back</Link>
+        </Grid>
+        <Grid item xs={4} container>
+          <Typography>Project1</Typography>
+          photo
+        </Grid>
+        <Grid item xs={5} container>
+          description
+        </Grid>
+      </Grid>
     </>
   );
 }
