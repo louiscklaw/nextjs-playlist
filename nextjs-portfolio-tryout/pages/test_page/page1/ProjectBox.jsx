a874c0c635350baae9c5ff1062a084df3f7bf240
louiscklaw <louiscklaw@users.noreply.github.com>
2022-09-23 19:41:31 +0800

    update nextjs-portfolio-tryout,

--- nextjs-portfolio-tryout/pages/test_page/page1/ProjectBox.jsx
+++ nextjs-portfolio-tryout/pages/test_page/page1/ProjectBox.jsx
@@ -1,11 +1,52 @@
-import { Box, Grid, Stack } from '@mui/material';
+import { Box, Grid, Stack, Typography } from '@mui/material';
+import Link from 'next/link';
+import test_project_background_avif from './test_project_background.avif';
 
 export default function ProjectBox() {
   return (
     <>
-      <Box style={{ padding: '10px', height: '100%' }}>
-        <Box style={{ height: '100%' }}>ProjectBox</Box>
-      </Box>
+      <Link href="/test_page/project1" passHref>
+        <Box style={{ padding: '1px', height: '100%', cursor: 'pointer' }}>
+          <Box style={{ height: '100%' }}>
+            <Box
+              style={{
+                height: '100%',
+                width: '100%',
+                backgroundImage: `url(${test_project_background_avif.src})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+              <Stack
+                style={{ height: '100%', width: '100%' }}
+                flexDirection="column"
+                justifyContent="flex-end"
+                alignItems="center"
+              >
+                <Stack
+                  style={{
+                    padding: '10px',
+                    width: '100%',
+
+                    background: 'rgba( 255, 255, 255, 0.25 )',
+                    boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+                    backdropFilter: 'blur( 4px )',
+                    border: '1px solid rgba( 255, 255, 255, 0.18 )',
+                  }}
+                  flexDirection="column"
+                  justifyContent="flex-end"
+                  alignItems="center"
+                >
+                  <Typography variant={'body1'} color="#ffffff">
+                    ProjectBox
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Box>
+          </Box>
+        </Box>
+      </Link>
     </>
   );
 }
---
+++ nextjs-portfolio-tryout/pages/test_page/project1/index.jsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+import Link from 'next/link';
+
+export default function Project1() {
+  return (
+    <>
+      <Typography>Project1</Typography>
+      <Link href="/test_page/page1">back</Link>
+    </>
+  );
+}
