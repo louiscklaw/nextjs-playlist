419ba517c30b3e79a87fcb696f6674cdeeaae436
louiscklaw <louiscklaw@users.noreply.github.com>
2022-09-25 03:33:00 +0800

    update nextjs-portfolio-tryout,

--- nextjs-portfolio-tryout/pages/test_page/about_louis/index.jsx
+++ nextjs-portfolio-tryout/pages/test_page/about_louis/index.jsx
@@ -4,6 +4,13 @@ import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
 import BottomLink from '../BottomLink';
 
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
 function BackButton() {
   return (
     <Link href="/test_page/page1" passHref>
@@ -37,6 +44,9 @@ export default function About() {
           alignItems="center"
         >
           <Box>
+            <Box>
+              <Typography variant="h5">En / 中</Typography>
+            </Box>
             <Box>name card ?</Box>
             <BackButton />
           </Box>
@@ -53,12 +63,32 @@ export default function About() {
           <Grid item xs={6}>
             <Box>
               <Stack spacing={5}>
-                <Typography variant="h3">About me</Typography>
+                <Typography variant="h5">Something about me</Typography>
                 <Stack spacing={2}>
-                  <Typography variant="h5">PURPOSE:</Typography>
+                  <Typography variant="h5">Language:</Typography>
                   <Typography variant="body1">
-                    A simple page to monitor stock, while data provided by
-                    trading view
+                    Cantonese, English, Mandarin
+                  </Typography>
+                </Stack>
+                <Stack spacing={2}>
+                  <Typography variant="h5">THINGS I DO WHEN I FREE:</Typography>
+                  <Typography variant="body1">
+                    Electronic stuff software (arduino, raspberry pi), hardware,
+                    Do some PCB layout Some making/modeling, Web page scraping
+                    github searching
+                  </Typography>
+                </Stack>
+
+                <Stack spacing={2}>
+                  <Typography variant="h5">OTHER SKILLS:</Typography>
+                  <Typography variant="body1">
+                    Linux management Web frontend
+                    (bulma/materialize.css/next/gatsby) SQL (MySQL/MariaDB),
+                    Realtime DB (Firebase) Desktop applications (kivy)
+                    Serverless (Firebase Cloud Functions), PaaS (Heroku) Docker
+                    with docker-compose, nginx (Load balancing + Failover)
+                    OCR/captcha study(tesseract) KVM (Performance optimization,
+                    GPU Passthrough by OVMF)
                   </Typography>
                 </Stack>
               </Stack>
@@ -66,13 +96,49 @@ export default function About() {
           </Grid>
           <Grid item xs={6}>
             <Box>
-              <Stack spacing={5}>
+              <Stack spacing={2}>
                 <Typography variant="h5">Work history</Typography>
-                <Stack spacing={2}>
-                  <Typography variant="body1">PURPOSE:</Typography>
-                  <Typography variant="body2">
-                    A simple page to monitor stock, while data provided by
-                    trading view
+
+                <Timeline
+                  sx={{
+                    [`& .${timelineItemClasses.root}:before`]: {
+                      flex: 0,
+                      padding: 0,
+                    },
+                  }}
+                >
+                  <TimelineItem>
+                    <TimelineSeparator>
+                      <TimelineDot />
+                      <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                      Handy - Hi. inc. Quality Assurance Engineer(Automation)
+                    </TimelineContent>
+                  </TimelineItem>
+                  <TimelineItem>
+                    <TimelineSeparator>
+                      <TimelineDot />
+                      <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                      Sierrawireless Validation engineer
+                    </TimelineContent>
+                  </TimelineItem>
+                  <TimelineItem>
+                    <TimelineSeparator>
+                      <TimelineDot />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                      china mobile hong kong Engineer(Radio access network)
+                    </TimelineContent>
+                  </TimelineItem>
+                </Timeline>
+
+                <Stack spacing={1}>
+                  <Typography variant="h5">EDUCATION BACKGROUND:</Typography>
+                  <Typography variant="body1">
+                    City University of Hong Kong (Electronic engineering)
                   </Typography>
                 </Stack>
               </Stack>
