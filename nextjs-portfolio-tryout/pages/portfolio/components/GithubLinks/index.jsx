0ba1028693606248d58716a8be9d8cdb46cbc226
louiscklaw <louiscklaw@users.noreply.github.com>
2022-09-27 16:28:02 +0800

    update nextjs-portfolio-tryout,

--- nextjs-portfolio-tryout/pages/portfolio/components/GithubLinks/index.jsx
+++ nextjs-portfolio-tryout/pages/portfolio/components/GithubLinks/index.jsx
@@ -1,3 +1,19 @@
+import { Stack, Typography } from '@mui/material';
+import Link from 'next/link';
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
+
 export default ({ href }) => {
-  return <>Github links : {href}</>;
+  return (
+    <>
+      <Link href={href} passHref>
+        <Stack direction="row" alignItems="center" spacing={0.5}>
+          <Typography variant={'body1'}>{href}</Typography>
+          <>
+            <FaGithub />
+            <FaExternalLinkAlt />
+          </>
+        </Stack>
+      </Link>
+    </>
+  );
 };
